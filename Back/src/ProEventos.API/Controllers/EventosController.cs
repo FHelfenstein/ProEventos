a0f8c61dc77a91c228c957aea6ddc9f10a770eb6
Fernando Helfenstein <fhelfenstein@concent.com.br>
2022-10-14 21:30:44 -0300

    Secao 06: .Net Core - Organizando as Camadas

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using ProEventos.API.Data;
-using ProEventos.API.Models;
+using ProEventos.Application;
+using ProEventos.Domain;
+using ProEventos.Persistence.Contextos;
 
 namespace ProEventos.API.Controllers
 {
@@ -14,42 +15,116 @@ namespace ProEventos.API.Controllers
     public class EventosController : ControllerBase
     {            
            
-        public readonly DataContext _context;
+        private readonly IEventoService _eventoService;
                       
-        public EventosController(DataContext context)
+        public EventosController(IEventoService eventoService)
         {
-            _context = context;
+            _eventoService = eventoService;
         }
         
         [HttpGet]
-        public IEnumerable<Evento> Get()
+        public async Task<IActionResult> Get()
         {
-            return _context.Eventos;
+            try
+            {
+                var eventos = await _eventoService.GetAllEventosAsync(true);
+                if(eventos == null) return NotFound("Nenhum evento encontrado.");
+
+                return Ok(eventos);
+            }
+            catch (Exception ex)
+            {
+                
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar recuperar eventos Erro: {ex.Message}");
+            }
         }
 
         [HttpGet("{id}")]
-        public Evento GetById(int id)
+        public async Task<IActionResult> GetById(int id)
         {
             //return _evento.Where(evento => evento.EventoId == id && evento.Local == local );
-            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+            //return _context.Eventos.FirstOrDefault(evento => evento.Id == id);
+            try
+            {
+                var evento = await _eventoService.GetEventoByIdAsync(id,true);
+                if(evento == null) return NotFound("Evento por Id não encontrado.");
+
+                return Ok(evento);
+            }
+            catch (Exception ex)
+            {
+                
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar recuperar evento Erro: {ex.Message}");
+            }            
+        }
+
+        [HttpGet("{tema}/tema")]
+        public async Task<IActionResult> GetByTema(string tema)
+        {
+            try
+            {
+                var eventos = await _eventoService.GetAllEventosByTemaAsync(tema,true);
+                if(eventos == null) return NotFound("Eventos por tema não encontrado.");
+
+                return Ok(eventos);
+            }
+            catch (Exception ex)
+            {                
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar recuperar eventos Erro: {ex.Message}");
+            }            
         }
         
         [HttpPost]
-        public string Post()
+        public async Task<IActionResult> Post(Evento model)
         {
-            return "Exemplo de POST por Fernando José Helfenstein";
+            //return "Exemplo de POST por Fernando José Helfenstein";
+            try
+            {
+                var evento = await _eventoService.AddEventos(model);
+                if(evento == null) return BadRequest("Erro ao tentar adicionar o evento.");
+
+                return Ok(evento);
+            }
+            catch (Exception ex)
+            {                
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar adicionar eventos Erro: {ex.Message}");
+            }              
         }
 
         [HttpPut("{id}")]
-        public string Put(int id)
+        public async Task<IActionResult> Put(int id, Evento model)
         {
-            return $"Exemplo de PUT por Fernando José Helfenstein id= {id}";
+            try
+            {
+                var evento = await _eventoService.UpdateEvento(id,model);
+                if(evento == null) return BadRequest("Erro ao tentar atualizar o evento.");
+
+                return Ok(evento);
+            }
+            catch (Exception ex)
+            {                
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar atualizar eventos Erro: {ex.Message}");
+            }
         }
 
         [HttpDelete("{id}")]
-        public string Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
-            return $"Exemplo de DELETE por Fernando José Helfenstein id= {id}";
+            //return $"Exemplo de DELETE por Fernando José Helfenstein id= {id}";
+            try
+            {
+                return await _eventoService.DeleteEvento(id) ? Ok("Deletado.") : BadRequest("Evento não deletado.");                
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar eliminar evento Erro: {ex.Message}");
+            }               
             
         }
 
---
+++ Back/src/ProEventos.Persistence/Contextos/ProEventosContext.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence.Contextos
+{
+    public class ProEventosContext : DbContext
+    {
+        public ProEventosContext(DbContextOptions<ProEventosContext> options) 
+            : base(options)  { }
+        public DbSet<Evento> Eventos { get; set; }
+        public DbSet<Lote> Lotes { get; set; }
+        public DbSet<Palestrante> Palestrantes { get; set; }
+        public DbSet<PalestranteEvento> PalestrantesEventos { get; set; }
+        public DbSet<RedeSocial> RedesSociais { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<PalestranteEvento>()
+                .HasKey(PE => new{PE.EventoId, PE.PalestranteId});
+
+            modelBuilder.Entity<Evento>()    
+                .HasMany(e => e.RedesSociais)  // um evento tem muitas redes sociais
+                .WithOne(rs => rs.Evento)      // cada rede social tem um evento
+                .OnDelete(DeleteBehavior.Cascade); // deletou o evento e esse evento possui redes sociais então delete em cascade
+
+            modelBuilder.Entity<Palestrante>()
+                .HasMany(e => e.RedesSociais)    
+                .WithOne(rs => rs.Palestrante)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+    }
+        
+}
